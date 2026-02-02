f89f4eb41cc3742b04f1573dafc0e71673d17840
paul <paul@mayflower.work>
2026-02-02 16:12:20 +0100

    [MOB-405] Changed fps range

--- encoder/src/main/java/com/pedro/encoder/input/video/Camera2ApiManager.kt
+++ encoder/src/main/java/com/pedro/encoder/input/video/Camera2ApiManager.kt
@@ -193,7 +193,7 @@ class Camera2ApiManager(context: Context) : CameraDevice.StateCallback() {
         val builderInputSurface = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
         for (surface in surfaces) builderInputSurface.addTarget(surface)
         builderInputSurface.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
-        val validFps = min(60, fps)
+        val validFps = min(15, fps)
         builderInputSurface.set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, Range(validFps, validFps))
         this.builderInputSurface = builderInputSurface
         return builderInputSurface.build()
